c0d3dcc6ef4b53deb1c7d4d0b572d509a45f872d
Jeff Lovell <jeff@lovellworks.com>
2020-03-20 17:30:41 -0400

    Support Creating New Todos - Refactor from click() to on()
    Added support for creating new todos with a setter method to clear existing text from input.
    
    refactored checkoff and delete functions from click() to on() pointing at parent element and targetting child element for manipulation of dynamically created content post page load.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 // Check Off Specific ToDos by Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
-})
+});
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
-})
+});
+
+//Create Todos
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//set value to an empty string
+		$(this).val("");
+		// create a new li and add to ul (using append())
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
