c0f3b951c7c6df98e538999eb51a38c78f83a81c
Jeff Lovell <jeff@lovellworks.com>
2020-03-20 17:08:25 -0400

    Clicking X to delete a todo
    using jQuery stopPropagation() to prevent event bubbling
    
    targeting a parent element as an object and deferring remove as a callback on fadeOut().

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 // Check Off Specific ToDos by Clicking
 $("li").click(function(){
 	$(this).toggleClass("completed");
-});
+})
+
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
